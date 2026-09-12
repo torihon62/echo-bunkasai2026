af88f376751f6115a05379396fd879291bf38385
torihon62 <torihon62@gmail.com>
2026-09-12 17:28:00 +0900

    情報の入力（図なし）

--- app/rururu/View.tsx
+++ app/rururu/View.tsx
@@ -5,75 +5,64 @@ import ContentContainer from "@/components/ContentContainer";
 import FirstView from "@/components/FirstView";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
-import SectionDevider from "@/components/SectionDevider";
 
 import footerImage from "@/components/assets/footer.png";
 import devider from "@/components/assets/section_divider_rururu.png";
 import ContentTitle from "@/components/ContentTitle";
-import Performers from "@/components/rururu/Performers";
 import TwoColumnContainer from "@/components/TwoColumnContainer";
 import { Box } from "@mui/material";
 import DetailList from "@/components/DetailList";
-import GuestPerformers from "@/components/rururu/GuestPerformers";
+import Form from "@/components/Form";
+import { useRef } from "react";
 
 export default function View() {
   const aboutMessages = [
-    "準備中",
     "",
+    "楽しいことの達人「チーム・るるる」です！",
+    "お芝居、歌、ヒーローショー！？",
+    "一緒におはなしを楽しみましょう♪",
+    "",
+    "・♡スイート回：公演終了後は工作コーナーもあるよ！",
+    "・♠️ビター回：お話や歌を甘さひかえめにアレンジしてお届け！",
+    "",
+    "ハロウィンの仮装も大歓迎♪",
+    "お子さまから大人まで、みんな集まれ〜！",
   ];
 
   const rururuSummaryLeft = [
     {
-      subHeader: "ワークショップ名",
+      subHeader: "イベントタイトル",
       contents: ["チーム・るるるのおはなしであそぼ！"],
     },
-    {
-      subHeader: "出演",
-      contents: [
-        "雨蘭 咲木子",
-        "田中 英樹",
-        "澤山 佳小里",
-        "葛谷 知花",
-        "加藤 拓二",
-        "おまたかな",
-        "橋爪 紋佳",
-        "佐藤 裕樹",
-        "大久保 彩乃",
-        "佐藤 巴南",
-        "新家 健太",
-        "安原 義人 (スペシャルゲスト)  ※ 3 回目のみ出演",
-        "後藤 敦 (スペシャルゲスト)",
-      ],
-    },
+    // {
+    //   subHeader: "出演",
+    //   contents: [
+    //     "雨蘭 咲木子",
+    //     "田中 英樹",
+    //     "澤山 佳小里",
+    //     "葛谷 知花",
+    //     "加藤 拓二",
+    //     "おまたかな",
+    //     "橋爪 紋佳",
+    //     "佐藤 裕樹",
+    //     "大久保 彩乃",
+    //     "佐藤 巴南",
+    //     "新家 健太",
+    //     "安原 義人 (スペシャルゲスト)  ※ 3 回目のみ出演",
+    //     "後藤 敦 (スペシャルゲスト)",
+    //   ],
+    // },
     {
       subHeader: "日時",
       contents: [
         "2026年10月10日 (土)",
-        "・ 14:00～15:30　スイート",
-        "・ 17:00～18:00　ビター",
-        "",
+        "① 14時～14時45分　（スイート♡）",
+        "② 17時～18時　（ビター♠️）",
+        "　",
         "2026年10月11日 (日)",
-        "・ 10:30～12:00　スイート",
-        "・ 14:00～15:30　スイート",
+        "③ 10時30分～11時15分　（スイート♡）",
+        "④ 14時～14時45分　（スイート♡）",
         "※ 開場は開演の15分前",
-      ],
-    },
-  ];
-  const rururuSummaryRight = [
-    {
-      subHeader: "定員",
-      contents: ["各回 30 人程度"],
-    },
-    {
-      subHeader: "対象年齢",
-      contents: ["どなたでも"],
-    },
-    {
-      subHeader: "参加費",
-      contents: [
-        "マルシェ入場料 500 円",
-        "※ 同会場開催の「ケイコバ de マルシェ」に入場できます",
-        "※ 会場に何度でも自由に出入りできます",
       ],
     },
     {
@@ -84,11 +73,41 @@ export default function View() {
         "(JR・地下鉄日比谷線 恵比寿駅より徒歩5分)",
       ],
     },
+  ];
+  const rururuSummaryRight = [
+    {
+      subHeader: "料金",
+      contents: [
+        "スイート回：小学生以下無料、中学生以上500円",
+        "ビター回：小学生以下無料、中学生以上1,000円",
+        "※ 当日現金精算のみ",
+      ],
+    },
+    {
+      subHeader: "所要時間",
+      contents: [
+        "スイート回：公演約45分＋終演後の工作タイム約30分",
+        "ビター回：公演約60分（工作タイムは無し）",
+      ],
+    },
+    {
+      subHeader: "参加方法",
+      contents: [
+        "・要事前申込（本ページのフォームにて申込）",
+        "・空きがあれば当日受付も可",
+      ],
+    },
     {
       subHeader: "お問い合わせ先",
       contents: [`echo.bunkasai2026<span class="contact" />`],
     },
   ];
+
+  const scrollRef = useRef<HTMLDivElement>(null);
+
+  const scroll = () => {
+    scrollRef?.current?.scrollIntoView({ behavior: "smooth" });
+  };
 
   return (
     <>
@@ -98,6 +117,7 @@ export default function View() {
         title="チーム・るるるのおはなしであそぼ！"
       />
       <FirstView
+        onClick={scroll}
         title="チーム・るるるのおはなしであそぼ！"
         // fileName="FirstView_party.png"
         background="#f2ebe6"
@@ -108,24 +128,24 @@ export default function View() {
         <About messages={aboutMessages} />
         <Box sx={{ pt: 5 }} />
       </ContentContainer>
-      {/* <SectionDevider imageSource={devider.src} />
-      <ContentContainer background="white">
-        <ContentTitle title="出演者" />
-        <Box sx={{ pb: { xs: 4, md: 8 } }} />
-        <Performers />
-      </ContentContainer>
-      <ContentContainer background="white">
-        <ContentTitle title="スペシャルゲスト" />
-        <Box sx={{ pb: { xs: 4, md: 8 } }} />
-        <GuestPerformers />
-      </ContentContainer>
       <ContentContainer background="#f6f6f6">
-        <ContentTitle title="ワークショップ詳細" />
+        <ContentTitle title="イベント詳細" />
         <TwoColumnContainer
           left={<DetailList summarys={rururuSummaryLeft} />}
           right={<DetailList summarys={rururuSummaryRight} />}
         />
-      </ContentContainer> */}
+      </ContentContainer>
+      <ContentContainer background="#f2ebe6">
+        <ContentTitle scrollRef={scrollRef} title="予約申し込みフォーム" />
+        <Box sx={{ display: "flex", justifyContent: "center" }}>
+          うまくスクロールできない方は
+          <a href="https://docs.google.com/forms/d/e/1FAIpQLSe_4LDtWSOQWGxDbks1WPik8VfV1hc6PaRuK_46GHdBAbE7tA/viewform?usp=pp_url&entry.723210304=0%E4%BA%BA&entry.1799826848=0%E4%BA%BA&entry.1692645899=0%E4%BA%BA&entry.1693144841=0%E4%BA%BA&entry.970210763=0%E4%BA%BA&entry.309687673=0%E4%BA%BA&entry.1268175974=0%E4%BA%BA&entry.1511099593=0%E4%BA%BA" target="_blank">
+            こちら
+          </a>
+          から
+        </Box>
+        <Form href="https://docs.google.com/forms/d/e/1FAIpQLSe_4LDtWSOQWGxDbks1WPik8VfV1hc6PaRuK_46GHdBAbE7tA/viewform?usp=pp_url&entry.723210304=0%E4%BA%BA&entry.1799826848=0%E4%BA%BA&entry.1692645899=0%E4%BA%BA&entry.1693144841=0%E4%BA%BA&entry.970210763=0%E4%BA%BA&entry.309687673=0%E4%BA%BA&entry.1268175974=0%E4%BA%BA&entry.1511099593=0%E4%BA%BA" />
+      </ContentContainer>
 
       <Footer imageSource={footerImage.src} />
     </>
--- components/Header.tsx
+++ components/Header.tsx
@@ -50,60 +50,21 @@ export default function Header(props: Props) {
   };
 
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar
-        position="static"
-        sx={{
-          boxShadow: "none",
-          background: props.background,
-          color: props.color,
-        }}
-      >
-        <Container maxWidth="xl">
-          <Toolbar disableGutters>
-            <Box sx={{ ":hover": { cursor: "pointer" } }}>
-              <Typography sx={{ fontWeight: "bold", fontSize: "large" }}>
-                {props.title}
-              </Typography>
-            </Box>
-            <Box sx={{ flexGrow: 1 }} />
-            <Box>
-              <IconButton
-                size="large"
-                edge="start"
-                aria-label="menu"
-                onClick={handleMenu}
-                sx={{ color: props.color, display: { xs: "flex" } }}
-              >
-                <MenuIcon />
-              </IconButton>
-              <Menu
-                anchorEl={anchorEl}
-                anchorOrigin={{
-                  vertical: "top",
-                  horizontal: "right",
-                }}
-                keepMounted
-                transformOrigin={{
-                  vertical: "top",
-                  horizontal: "right",
-                }}
-                open={Boolean(anchorEl)}
-                onClose={handleClose}
-              >
-                <MenuItem onClick={goToHome}>ホームへ戻る</MenuItem>
-                <MenuItem onClick={goToRururu}>
-                  チーム・るるるのおはなしパーティ
-                </MenuItem>
-                <MenuItem onClick={goToMarche}>ケイコバ de マルシェ</MenuItem>
-                <MenuItem onClick={goToDokidoki}>
-                  スタジオ de 声優チャレンジ
-                </MenuItem>
-              </Menu>
-            </Box>
-          </Toolbar>
-        </Container>
-      </AppBar>
-    </Box>
+    <AppBar
+      position="static"
+      sx={{
+        boxShadow: "none",
+        background: props.background,
+        color: props.color,
+      }}
+    >
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          <Typography sx={{ fontWeight: "bold", fontSize: "large" }}>
+            {props.title}
+          </Typography>
+        </Toolbar>
+      </Container>
+    </AppBar>
   );
 }
